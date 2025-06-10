abb60e255094ac933da3ce2bcefeb982d115d638
zrghr1 <harjucassius@gmail.com>
2025-06-11 01:38:56 +0300

    fixed typo

--- src/utils/TextureManager.cpp
+++ src/utils/TextureManager.cpp
@@ -2,7 +2,7 @@
 
 SDL_Texture* TextureManager::LoadTexture(const char* texture, SLD_Renderer* ren){
   SDL_Surface* tmpSurface = IMG_Load(texture);
-  SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSurface);
+  SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tmpSurface);
   SDL_DestroySurface(tmpSurface);
 
   return tex;
