0bf87f7ad809da4bc4e39f32208c55bd3fdb5472
zrghr1 <harjucassius@gmail.com>
2025-06-11 14:15:16 +0300

    move function for ball

--- src/gameobjects/Ball.cpp
+++ src/gameobjects/Ball.cpp
@@ -12,5 +12,16 @@ void Ball::move( SDL_FRect& wall){
   xpos += xvel;
   collider.x = xpos;
 
-  if ((xpos < 0 ) || (xpos + collider.w > 800))
+  if ((xpos < 0 ) || (xpos + collider.w > 800) || checkCollision(collider, wall)){
+    xpos -= xvel;
+    collider.x = xpos;
+  }
+
+  ypos += yvel;
+  collider.y = ypos;
+
+  if ((ypos < 0 ) || (ypos + collider.h > 800) || checkCollision(collider, wall)){
+    ypos -= yvel;
+    collider.y = ypos;
+  }
 }
