5d92aaf9579ed175baa790165b7c6605f0a58ec7
zrghr1 <harjucassius@gmail.com>
2025-06-11 14:21:29 +0300

    collision

--- src/utils/Collision.cpp
+++ src/utils/Collision.cpp
@@ -1,2 +1,35 @@
 #include "Collision.hpp"
 
+bool checkCollision( SDL_FRect a, SDL_FRect b ){
+  float leftA, leftB;
+  float rightA, rightB;
+  float topA, topB;
+  float bottomA, bottomB;
+
+  // Calculate sides of both colliders
+  leftA = a.x;
+  rightA = a.x + a.w;
+  topA = a.y;
+  bottomA = a.y + a.h;
+ 
+  leftB = b.x;
+  rightB = b.x + b.w;
+  topB = b.y;
+  bottomB = b.y + b.h;
+  
+  //Check Collision
+  if(bottomA <= topB){
+    return false;
+  }
+  if(topA >= bottomB){
+    return false;
+  }
+  if(rightA <= leftB){
+    return false;
+  }
+  if(leftA >= leftB){
+    return false;
+  }
+
+  return true;
+}
